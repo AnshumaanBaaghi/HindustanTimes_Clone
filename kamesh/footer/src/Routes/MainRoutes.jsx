4e14fe8314aee4723c7d7d0ee131ecd3dd328f15
kamesh1126 <96121369+kamesh1126@users.noreply.github.com>
2022-05-05 16:23:34 +0530

    done

---
+++ kamesh/footer/src/Routes/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import {Footer,Footer1} from "../Footercomponents/Footer";
+
+
+
+const MainRoutes = () => {
+  return <>{/* Navbar and all the routes */}
+   <Footer/>
+   <Footer1/>
+  
+  </>;
+};
+export { MainRoutes };
